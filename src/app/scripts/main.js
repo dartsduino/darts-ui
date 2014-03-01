e5f3a02f886e6b852cb3c730b12a2c9eae3c80da
Ikuo Terado <eqobar@gmail.com>
2014-03-01 21:35:10 +0900

    Add js code

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+/* global DartsUi */
+
+'use strict';
+
+function main () {
+  var dartsUi = new DartsUi('#darts-ui');
+
+  // dartsUi.focus(13, '1-o');
+  // dartsUi.blur(13, '1-o');
+  // dartsUi.focus(13, '1-i');
+  // dartsUi.focus(13, '2');
+  // dartsUi.focus(13, '3');
+
+  // dartsUi.calibrate();
+
+  dartsUi.onHit(function (cellId, point, ratio) {
+    console.log(cellId + ' : ' + point + ' x ' + ratio + ' = ' + point * ratio);
+  });
+}
